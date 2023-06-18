00a7abe78a7bece32d0435c3ad13d82c72affb38
Kris! <42949874+krstphrrr@users.noreply.github.com>
2023-06-17 18:42:43 -0600

    added inventory list placeholder

---
+++ app/src/main/java/com/example/perfumetest6/inventorycomponents/inventorylist.kt
@@ -0,0 +1,17 @@
+package com.example.perfumetest6.inventorycomponents
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+// TODO: which list approach allows me to add and remove? Recyclerview?
+// TODO: Nested navigation Home/InventoryList -> Ingredient; pass state to navbar
+// TODO: Composable screen for each Item should be dynamically generated
+// TODO: Items themselves should be stored in local DB (Room?)
+@Composable
+fun InventoryList() {
+}
+
+@Preview(showBackground = true)
+@Composable
+fun InventoryListPreview(){
+    InventoryList()
+}
